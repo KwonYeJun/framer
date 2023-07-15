55ba7dab571baf4529853726601c2472afd8733e
gwonyejun <kyjuni1059@gmail.com>
2023-07-15 15:19:46 +0900

    useEffect() 사용하여 3가지의 경우의수 공부 해보기

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 
 import './App.css';
 
 function App() {
   const [nickName,setNickName] = useState('주늬요');
 
+  useEffect(()=>{
+  console.log("마운트가 되었을 때");
+  })
+
+  useEffect(()=>{
+    console.log("의존성 빈 배열 버전");
+    },[])
+    
+    useEffect(()=>{
+      console.log("의존성 배열에 값을 넣은 버전");
+      },[nickName])
+      
   const togle = () => {
     setNickName('행복한 원숭이')
   }
@@ -12,6 +24,7 @@ function App() {
   return (
     <div className="App">
       <button onClick={togle}> {nickName}</button>
+    
     </div>
   );
 }
