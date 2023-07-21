77916a8d9b756b2163a7148eda3fdf9f95697694
gwonyejun <kyjuni1059@gmail.com>
2023-07-21 20:06:26 +0900

    상태 관리 라이브러리 연구해 보기

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,60 @@
-import React,{useState,useEffect} from 'react';
+// import React,{useState,useEffect} from 'react';
 
-import './App.css';
+// import './App.css';
 
-function App() {
-  const [nickName,setNickName] = useState('주늬요');
+// function App() {
+//   const [nickName,setNickName] = useState('주늬요');
 
-  useEffect(()=>{
-  console.log("마운트가 되었을 때");
-  })
+//   useEffect(()=>{
+//   console.log("마운트가 되었을 때");
+//   })
 
-  useEffect(()=>{
-    console.log("의존성 빈 배열 버전");
-    },[])
+//   useEffect(()=>{
+//     console.log("의존성 빈 배열 버전");
+//     },[])
     
-    useEffect(()=>{
-      console.log("의존성 배열에 값을 넣은 버전");
-      },[nickName])
+//     useEffect(()=>{
+//       console.log("의존성 배열에 값을 넣은 버전");
+//       },[nickName])
       
-  const togle = () => {
-    setNickName('행복한 원숭이')
-  }
+//   const togle = () => {
+//     setNickName('행복한 원숭이')
+//   }
+
+//   return (
+//     <div className="App">
+//       <button onClick={togle}> {nickName}</button>
+    
+//     </div>
+//   );
+// }
+
+import React from 'react';
+import useSWR, { SWRResponse } from 'swr';
+
+interface Item {
+  id: number;
+  name: string;
+}
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const App: React.FC = () => {
+  const { data, error }: SWRResponse<Item[], any> = useSWR('/api/data', fetcher, {
+    refreshInterval: 5000, // 5 seconds
+    revalidateOnFocus: true,
+  });
+
+  if (error) return <div>Failed to load data</div>;
+  if (!data) return <div>Loading...</div>;
 
   return (
-    <div className="App">
-      <button onClick={togle}> {nickName}</button>
-    
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
